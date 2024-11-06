c855b2ee328496fed6a8a3bd055f5414c1caf01f
PJ Bhakta <67594832+pjbhakta-outlook@users.noreply.github.com>
2024-11-06 17:18:51 +0000

    Refactor VSCode configuration and streamline Swagger setup in WebAPI

--- src/ContosoSuitesWebAPI/Program.cs
+++ src/ContosoSuitesWebAPI/Program.cs
@@ -65,11 +65,10 @@ builder.Services.AddSingleton<AzureOpenAIClient>((_) =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
